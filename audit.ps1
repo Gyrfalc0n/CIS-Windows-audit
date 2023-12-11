583a5d05e4d4242976e355b82f517c85bf7e9868
Gyrfalcon <46728024+Gyrfalc0n@users.noreply.github.com>
2023-12-11 23:01:48 +0100

    Update audit.ps1

--- audit.ps1
+++ audit.ps1
@@ -1,3 +1,8 @@
+function Write-Double($text) {
+    Write-Host $text
+    ResultToString($text)
+}
+
 function ShowGeneralInformation {
     # Hardware Model
     $hardwareModel = Get-WmiObject Win32_ComputerSystem | Select-Object Model
@@ -23,60 +28,203 @@ function ShowGeneralInformation {
         }
     }
     $wifiInformation | Format-Table -AutoSize
-    Write-Host $wifiInformation
+    Write-Double $wifiInformation
     # OS Version, Installation Date
     $osVersion = Get-CimInstance Win32_OperatingSystem | Select-Object Version
     $installDate = Get-CimInstance Win32_OperatingSystem | Select-Object InstallDate
-    Write-Host "`n"
-    Write-Host "======= General System Information ======="
-    Write-Host "Hardware Model: $($hardwareModel.Model)"
-    Write-Host "BIOS/UEFI Version: $($biosVersion.Version)"
-    Write-Host "`n======= RAM Information ======="
+    Write-Double "`n"
+    Write-Double "======= General System Information ======="
+    Write-Double "Hardware Model: $($hardwareModel.Model)"
+    Write-Double "BIOS/UEFI Version: $($biosVersion.Version)"
+    Write-Double "`n======= RAM Information ======="
     # adapt size to human readable format
     $ramSlots | ForEach-Object { $_.Capacity = [math]::Round($_.Capacity / 1GB, 2) }
-    $ramSlots | ForEach-Object { Write-Host "Slot: $($_.DeviceLocator), Size: $($_.Capacity) GB" }
-    Write-Host "`n======= Processor Information ======="
+    $ramSlots | ForEach-Object { Write-Double "Slot: $($_.DeviceLocator), Size: $($_.Capacity) GB" }
+    Write-Double "`n======= Processor Information ======="
     $processorInfoTable = $processorInfo | Format-Table -AutoSize | Out-String
-    Write-Host $processorInfoTable
+    Write-Double $processorInfoTable
 
-    Write-Host "`n======= Network Adapter Information ======="
+    Write-Double "`n======= Network Adapter Information ======="
     $networkAdaptersTable = $networkAdapters | Format-Table -AutoSize | Out-String
-    Write-Host $networkAdaptersTable
+    Write-Double $networkAdaptersTable
 
-    Write-Host "`n======= Hard Drive Information (GB) ======="
+    Write-Double "`n======= Hard Drive Information (GB) ======="
     $hardDrives | ForEach-Object { $_.Size = [math]::Round($_.Size / 1GB, 2) }
     $hardDrivesTable = $hardDrives | Format-Table -AutoSize | Out-String
-    Write-Host $hardDrivesTable
+    Write-Double $hardDrivesTable
 
-    Write-Host "`n======= BitLocker Information ======="
+    Write-Double "`n======= BitLocker Information ======="
     $bitlockerInfoTable = $bitlockerInfo | Format-Table -AutoSize | Out-String
-    Write-Host $bitlockerInfoTable
+    Write-Double $bitlockerInfoTable
 
-    Write-Host "`n======= WiFi Network Information ======="
+    Write-Double "`n======= WiFi Network Information ======="
     foreach ($wifiInfo in $wifiInformation) {
-        Write-Host "Network: $($wifiInfo.Network)"
-        Write-Host "Password: $($wifiInfo.Password)`n"
+        Write-Double "Network: $($wifiInfo.Network)"
+        Write-Double "Password: $($wifiInfo.Password)`n"
     }
-    Write-Host "`n======= OS Information ======="
-    Write-Host "OS Version: $($osVersion.Version)"
-    Write-Host "OS Installation Date: $($installDate.InstallDate)"
-    Write-Host "========================================="
+    Write-Double "`n======= OS Information ======="
+    Write-Double "OS Version: $($osVersion.Version)"
+    Write-Double "OS Installation Date: $($installDate.InstallDate)"
+    Write-Double "========================================="
 }
+
+function ShowUserInformation { # Show user information
+    # Display user account information
+    Write-Double "`n======= User Account Information ======="
+    $userAccounts = Get-WmiObject Win32_UserAccount
+    foreach ($account in $userAccounts) {
+        Write-Double "User Account: $($account.Caption)"
+        Write-Double "SID: $($account.SID)"
+        Write-Double "Full Name: $($account.FullName)"
+        Write-Double "Description: $($account.Description)"
+        Write-Double "Account Type: $($account.AccountType)"
+        Write-Double "Disabled: $($account.Disabled)"
+        Write-Double "Local Account: $($account.LocalAccount)"
+        Write-Double "Lockout: $($account.Lockout)"
+        Write-Double "Password Changeable: $($account.PasswordChangeable)"
+        Write-Double "Password Expires: $($account.PasswordExpires)"
+        Write-Double "Password Required: $($account.PasswordRequired)"
+        Write-Double "Password Age: $($account.PasswordAge)`n"
+    }
+
+    # Display SAM database dump (This is just an example, and it requires proper permissions)
+    Write-Double "`n======= SAM Database Dump ======="
+    $samDump = Get-Content 'C:\Windows\System32\config\SAM' -Encoding Byte -Raw
+    Write-Double "SAM Database Dump: $($samDump)`n"
+
+    # Display GPO information
+    Write-Host "`n======= GPO Information ======="
+    try {
+        $gpoInfo = Get-GPO -All
+        foreach ($gpo in $gpoInfo) {
+            Write-Double "GPO: $($gpo.DisplayName)"
+            Write-Double "ID: $($gpo.Id.Guid)"
+            Write-Double "Security Settings: $($gpo.SecurityDescriptor)"
+            Write-Double "WMI Filter: $($gpo.WmiFilterId)"
+            Write-Double "Creation Time: $($gpo.CreationTime)"
+            Write-Double "Modification Time: $($gpo.ModificationTime)`n"
+        }
+    } catch {
+        Write-Double "Error retrieving GPO information: $_`n"
+    }
+
+    # Verify UAC settings
+    Write-Double "`n======= Verify UAC Settings ======="
+    $uacSettings = Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System' -Name 'EnableLUA'
+    Write-Double "UAC Settings - EnableLUA: $($uacSettings.EnableLUA)`n"
+
+    Write-Double "`n========================================="
+}
+
+function ShowWindowsFirewallInformation {
+    Write-Double "`n======= Windows Firewall Settings ======="
+    
+    # Check if the Windows Firewall service is running
+    $firewallService = Get-Service -Name 'MpsSvc'
+    if ($firewallService.Status -eq 'Running') {
+        Write-Double "Windows Firewall Service is running.`n"
+
+        # Check if the firewall is enabled
+        $firewallStatus = Get-NetFirewallProfile | Select-Object Name, Enabled
+        foreach ($profile in $firewallStatus) {
+            Write-Double "`nFirewall Profile: $($profile.Name)"
+            Write-Double "Enabled: $($profile.Enabled)"
+        }
+
+        # Display inbound and outbound rules
+        Write-Double "`n======= Inbound Firewall Rules ======="
+        $inboundRules = Get-NetFirewallRule | Where-Object { $_.Direction -eq 'Inbound' } | Format-Table -AutoSize
+        if ($inboundRules.Count -eq 0) {
+            Write-Double "No Inbound Firewall Rules found.`n"
+        } else {
+            $inboundRules
+        }
+
+        Write-Double "`n======= Outbound Firewall Rules ======="
+        $outboundRules = Get-NetFirewallRule | Where-Object { $_.Direction -eq 'Outbound' } | Format-Table -AutoSize
+        if ($outboundRules.Count -eq 0) {
+            Write-Double "No Outbound Firewall Rules found.`n"
+        } else {
+            $outboundRules
+        }
+
+        # CIS Recommendation: Ensure 'Windows Firewall: Public: Firewall state' is set to 'On' (1)
+        $publicFirewallState = (Get-NetFirewallProfile -Name 'Public').Enabled
+        if ($publicFirewallState -ne 1) {
+            Write-Double "`nCIS Recommendation: Windows Firewall Public Profile should be set to 'On'.`n"
+        }
+    } else {
+        Write-Double "`nWindows Firewall Service is not running.`n"
+    }
+
+    Write-Double "`n========================================="
+}
+
+function AutoDiscoverMinimizationServices {
+    Write-Double "`n======= Auto-Discover Minimization Services ======="
+
+    # Criteria for automatically adding services to the list
+    $criteria = @{
+        # Add criteria according to your needs
+        'Status' = 'Running'  # Only running services
+        'StartType' = 'Automatic'  # Only services with automatic start
+    }
+
+    # Get services that meet the criteria
+    $servicesToStop = Get-Service | Where-Object {
+        $matchesAll = $true
+        foreach ($key in $criteria.Keys) {
+            $matchesAll = $matchesAll -and $_.$key -eq $criteria[$key]
+        }
+        $matchesAll
+    } | Select-Object -ExpandProperty Name
+
+    foreach ($service in $servicesToStop) {
+        $serviceStatus = Get-Service -Name $service -ErrorAction SilentlyContinue
+        if ($serviceStatus -ne $null) {
+            $color = 'White'  # Default color
+
+            # Add additional conditions for highlighting as needed
+            if ($serviceStatus.Status -eq 'Running') {
+                $color = 'Red'  # Highlight running services
+            }
+
+            Write-Double "Service: $($serviceStatus.DisplayName)"
+            Write-Double "Status: $($serviceStatus.Status)`n" -ForegroundColor $color
+        } else {
+            Write-Double "Service $service not found.`n"
+        }
+    }
+
+    Write-Double "`n========================================="
+}
+
+
 
 function AuditCIS {  # Audit the system according to CIS
     $cisScriptPath = Join-Path $PSScriptRoot "cis.ps1"
     if (Test-Path $cisScriptPath) {
         & $cisScriptPath
     } else {
-        Write-Host "Error: cis.ps1 not found at $cisScriptPath."
+        Write-Double "Error: cis.ps1 not found at $cisScriptPath."
     }
 }
+
+function ResultToString($result) {  # Append result to global variable
+    $global:results += $result
+}
+
+
 
 function ShowMenu { # Show menu
     Clear-Host
     Write-Host "Menu:"
     Write-Host "1. Show general information about the machine"
-    Write-Host "2. Audit the system according to CIS"
+    Write-Host "2. Show user information"
+    Write-Host "3. Show Windows Firewall information"
+    Write-Host "4. Show minimization services"
+    Write-Host "5. Audit the system according to CIS"
+    Write-Host "O. Open results in Notepad"
     Write-Host "Q. Quit"
 }
 
@@ -87,33 +235,42 @@ function ShowSubMenu1 { # Show submenu 1
 }
 
 # MAIN
+# instantiate global variable
+$global:results = @()
+
 while ($true) {
     ShowMenu
     $choice = Read-Host "Enter your choice number"
 
     switch ($choice) {
         '1' {
-            $informationArray = ShowGeneralInformation
-            ShowSubMenu1
-            $choice1 = Read-Host "Enter your choice number"
-            switch ($choice1){
-                '1' {
-                    ExportInfoToHTML($informationArray)
-                    Pause
-                    break
-                }
-                '2' {
-                    break
-                }
-                default {
-                    Write-Host "Invalid choice. Please enter a valid number."
-                    Start-Sleep -Seconds 2
-                }
-            }
+            ShowGeneralInformation
+            Pause
+            break
+        }
+        '5' {
+            AuditCIS
+            break
+        }
+        '4' {
+            AutoDiscoverMinimizationServices
+            Pause
+            break
+        }
+        '3' {
+            ShowWindowsFirewallInformation
+            Pause
             break
         }
         '2' {
-            AuditCIS
+            ShowUserInformation
+            Pause
+            break
+        }
+        'O' {
+            Write-Host "Opening results in Notepad..."
+            $global:results | Out-File -FilePath 'results.txt'
+            notepad 'results.txt'
             break
         }
         'Q' {
